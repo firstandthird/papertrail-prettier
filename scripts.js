37a8691ad52456d7dfe8b6dd733b6638d1377530
Greg Allen <greg@firstandthird.com>
2017-08-09 20:50:19 -0700

    initial work

---
+++ scripts.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+document.querySelector('[value=truncate]').parentNode.click();
+
+}, 5000);
+//document.querySelector('[value=hide_system]').click();
+//document.querySelector('[value=hide_time]').click();
+//document.querySelector('#event_list').classList.add('truncate', 'hide-system', 'hide-time');
